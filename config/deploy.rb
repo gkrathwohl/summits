af5b81ae30fa5f9ae72037582f4c6e6370816663
Greg Krathwohl <gkrathwohl@gmail.com>
2016-08-16 15:19:20 -0400

    bin cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,11 @@ set :repo_url, 'git@github.com:gkrathwohl/summits.git'
 set :deploy_to, '/home/deploy/summits'
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# removed bin
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 
 namespace :deploy do
 
