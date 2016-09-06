7614e7c57bc385c1081950712015d842ba6f8efd
Greg Krathwohl <gkrathwohl@gmail.com>
2016-09-06 13:22:50 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ set :application, 'summits'
 set :repo_url, 'git@github.com:gkrathwohl/summits.git'
 
 set :deploy_to, '/home/gkrathwohl/summits'
+set :default_env, { path: "/home/gkrathwohl/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 #set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
