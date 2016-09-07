de35f679936e7bdbac3a483a0233ec92d7e4b04f
Greg Krathwohl <gkrathwohl@gmail.com>
2016-09-06 20:52:42 -0400

    debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def connect
 
-    logger.debug "connectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnectingconnecting"
+    logger.debug "!!!!!!!  connecting !!!!!!!!!!!"
 
     client_id = "3764"
     client_secret = "e0b897e6bc461b774c73fbff6936f656d2e376f3"
