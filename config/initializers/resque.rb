2b2ded77f8f2913fdbeb679fc83fc4e5b9e9aeb4
Greg Krathwohl <gkrathwohl@gmail.com>
2017-03-27 22:02:52 -0400

    resque logging

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,6 @@ if ENV["REDIS_URL"]
     $redis = Redis.new(:url => ENV["REDIS_URL"])
     Resque.redis = $redis
 end
+
+Resque.logger = Logger.new("#{Rails.root}/log/resque.log")
+Resque.logger.formatter = Resque::VeryVerboseFormatter.new
