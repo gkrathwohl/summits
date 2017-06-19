76b8a8dde75c198fdcbeb7890ff0ed876dd18fae
Greg Krathwohl <gkrathwohl@gmail.com>
2017-06-19 11:11:52 -0400

    azure-db

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 set :application, 'summits'
 set :repo_url, 'git@github.com:gkrathwohl/summits.git'
 
-set :deploy_to, '/home/deploy/summits'
+set :deploy_to, '/home/gkrathwohl/summits'
 
-set :default_env, { path: "/home/deploy/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :default_env, { path: "/home/gkrathwohl/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
